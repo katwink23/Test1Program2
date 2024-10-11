84c1626ad5a98d04850a7480265e3870c11d6716
Katlyn W. <krw8ateez@gmail.com>
2024-10-11 10:39:47 -0400

    Add project files.

---
+++ Test1Program2.cpp
@@ -0,0 +1,18 @@
+// Test1Program2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Ka
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    
+
+
+
+
+}
+
